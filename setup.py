f855cd60ff153d194cebd1897a73494b173bb0b0
Francis Sun <francisjsun@outlook.com>
2019-04-04 12:51:58 +0800

    update version

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(name="mars",
-                 version="0.1.0",
-                 # author="Francis Suen",
-                 # author_email="francissuen@fs.com",
+                 version="0.0.0",
+                 author="Francis Suen",
+                 author_email="francissuen@outlook.com",
                  description="A python utils package.",
                  long_description=long_description,
                  long_description_content_type="text/markdown",
